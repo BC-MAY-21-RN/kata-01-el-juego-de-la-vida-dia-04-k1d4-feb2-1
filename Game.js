a6b62ee0c16922e39d9472b1e2272ffb89aa7a54
Jesus <chuchober@hotmail.com>
2022-02-14 18:23:06 -0500

    Add  countLiveNeighbors method and init test
    Co-authored-by: caramonp <caramonp@users.noreply.github.com>
    Co-authored-by: Oscar Villarraga  <osvilla27@users.noreply.github.com>
    Co-authored-by: Mario Torres <tmario1508@users.noreply.github.com>

--- Game.js
+++ Game.js
@@ -1,6 +1,6 @@
-const Grid = require('./Grid');
-class Game {
-  constructor(rows, columns) {
-    this.grid = new Grid(rows, columns);
-  }
-}
+// const Grid = require('./Grid');
+// class Game {
+//   constructor(rows, columns) {
+//     this.grid = new Grid(rows, columns);
+//   }
+// }
